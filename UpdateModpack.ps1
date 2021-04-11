1c2222582930a975622522e70776be2ed3ccc879
Nicholas Dechert <dechert.nicholas@gmail.com>
2021-04-10 18:12:28 -0700

    First version of update script and files

--- UpdateModpack.ps1
+++ UpdateModpack.ps1
@@ -1,28 +1,34 @@
 # Set some constants
-$BUILD_DIR = Resolve-Path .\buildFolder
-$OUT_DIR = Resolve-Path .\releaseOut
-$CACHE_DIR = Resolve-Path .\downloadCache
-$PACK_NAME = "ProfJam's RLCraft +"
 $REPO = "DechertNicholas/ProfJamRLCraftPlus"
-$FILE = "ProfJamsRLCraftPlus"
+$FILE = "ProfJamsRLCraft+"
+$SCRIPT_TEMP = ".\_UpdateScript"
+$SCRIPT_DOWNLOAD = "$SCRIPT_TEMP\Download"
+$SCRIPT_EXTRACT = "$SCRIPT_TEMP\Extracted"
 
-# Create DIRs
-if (!(Test-Path $BUILD_DIR)) {
-    New-Item -ItemType Directory -Name $BUILD_DIR
+# make script dir
+if (!(Test-Path $SCRIPT_DOWNLOAD)) {
+    New-Item -ItemType Directory -Name $SCRIPT_DOWNLOAD -Force
 }
-if (!(Test-Path $OUT_DIR)) {
-    New-Item -ItemType Directory -Name $OUT_DIR
-}
-if (!(Test-Path $CACHE_DIR)) {
-    New-Item -ItemType Directory -Name $CACHE_DIR
-}
-Pause
+
 # Get and download the latest release into the cache
 # https://gist.github.com/MarkTiedemann/c0adc1701f3f5c215fc2c2d5b1d5efd3
+
 $releases = "https://api.github.com/repos/$repo/releases"
-$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$tag = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
 $versionedName = $FILE + $tag + ".zip"
 $download = "https://github.com/$repo/releases/download/$tag/$versionedName"
 
+Invoke-WebRequest $download -OutFile "$SCRIPT_DOWNLOAD\$versionedName"
+
+Expand-Archive -Path "$SCRIPT_DOWNLOAD\$versionedName" -DestinationPath "$SCRIPT_EXTRACT\$($FILE + $tag)"
+
+$modlist = Get-Content "$SCRIPT_EXTRACT\$($FILE + $tag)\AddedMods.json" | ConvertFrom-Json
+foreach ($mod in $modlist.mods) {
+    Write-Host "Processing $($mod.Name)..."
+    Invoke-WebRequest "$($mod.url)" -OutFile ".\mods\$($mod.fileName)"
+}
+Write-Host "Copying modified configs..."
+Copy-Item -Path "$SCRIPT_EXTRACT\$($FILE + $tag)\config" -Destination .\config -Recurse -Force
+
 # Zip the pack
-& 'C:\Program Files\7-Zip\7z.exe' a "$OUT_DIR\$PACK_NAME.zip" "$BUILD_DIR\*"
+#& 'C:\Program Files\7-Zip\7z.exe' a "$OUT_DIR\$($PACK_NAME + $tag).zip" "$BUILD_DIR\*"
