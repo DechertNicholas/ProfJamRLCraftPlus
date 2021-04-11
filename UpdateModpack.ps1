1ee35e1a6d99e5dd9e337f8b27204d716ee89ab1
Nicholas Dechert <dechert.nicholas@gmail.com>
2021-04-10 18:27:16 -0700

    Complete the update script

--- UpdateModpack.ps1
+++ UpdateModpack.ps1
@@ -9,6 +9,13 @@ $SCRIPT_EXTRACT = "$SCRIPT_TEMP\Extracted"
 if (!(Test-Path $SCRIPT_DOWNLOAD)) {
     New-Item -ItemType Directory -Name $SCRIPT_DOWNLOAD -Force
 }
+if (!(Test-Path $SCRIPT_EXTRACT)) {
+    New-Item -ItemType Directory -Name $SCRIPT_EXTRACT -Force
+}
+# no reason this should ever not exist but uhhhh.... just in case
+if (!(Test-Path ".\mods")) {
+    New-Item -ItemType Directory -Name ".\mods" -Force
+}
 
 # Get and download the latest release into the cache
 # https://gist.github.com/MarkTiedemann/c0adc1701f3f5c215fc2c2d5b1d5efd3
@@ -22,13 +29,18 @@ Invoke-WebRequest $download -OutFile "$SCRIPT_DOWNLOAD\$versionedName"
 
 Expand-Archive -Path "$SCRIPT_DOWNLOAD\$versionedName" -DestinationPath "$SCRIPT_EXTRACT\$($FILE + $tag)"
 
+# download all the mods
 $modlist = Get-Content "$SCRIPT_EXTRACT\$($FILE + $tag)\AddedMods.json" | ConvertFrom-Json
 foreach ($mod in $modlist.mods) {
     Write-Host "Processing $($mod.Name)..."
     Invoke-WebRequest "$($mod.url)" -OutFile ".\mods\$($mod.fileName)"
 }
-Write-Host "Copying modified configs..."
-Copy-Item -Path "$SCRIPT_EXTRACT\$($FILE + $tag)\config" -Destination .\config -Recurse -Force
 
-# Zip the pack
-#& 'C:\Program Files\7-Zip\7z.exe' a "$OUT_DIR\$($PACK_NAME + $tag).zip" "$BUILD_DIR\*"
+# copy edited config files and overwrite old ones
+Write-Host "Copying modified configs..."
+Copy-Item -Path "$SCRIPT_EXTRACT\$($FILE + $tag)\config" -Destination .\ -Recurse -Force
+
+Write-Host "Completed!"
+Write-Host "Report any errors to Valyrin on discord, or post an issue in the Github"
+Write-Host "Press 'enter' to exit this updater. You should be good to join the server."
+pause
